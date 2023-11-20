3439d1f0c50f5794b49fbb2e6a36175128df45c5
Binnukrishna <binnukrishna5233@gmail.com>
2023-11-20 14:32:49 +0530

    basic sign in sign out success

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,48 @@
 import { useState, useEffect } from "react";
-import { app } from "../firebase/fire_config";
+import { auth } from "../firebase/fire_config";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    const unSubscribe = app.auth().onAuthStateChanged((user) => {
+    const listen = onAuthStateChanged(auth, (user) => {
       if (user) {
-        setUserData(user.email);
+        setUserData(user);
       } else {
         setUserData(null);
       }
     });
-    return () => unSubscribe();
+    return () => listen();
   }, []);
 
   const logOut = () => {
     console.log(" Need to implement ");
+
+    signOut(auth)
+      .then(() => {
+        console.log("Sign out success.");
+      })
+      .catch((err) => {
+        alert(err.message);
+        console.log(err.code);
+      });
   };
 
   return (
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center font-sans font-extrabold text-2xl">
       {userData ? (
-        <p>You are logged in as : {userData}</p>
+        <p>You are logged in as : {userData.email}</p>
       ) : (
-        <p>You are not Logined ..!</p>
+        <>
+          <p>You are not Logined ..!</p>
+          <Link to={"/login"}>
+            <button className=" text-lg text-black font-normal border-2 border-black w-20 h-10 rounded-lg hover:bg-black hover:text-white transition-all duration-300 ">
+              Login
+            </button>
+          </Link>
+        </>
       )}
 
       {userData && (
